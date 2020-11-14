10ba2ca4df9627ce1b64fc24e04326c8f576306f
Omkar Sarde <72186361+omkarsarde@users.noreply.github.com>
2020-11-14 00:46:11 -0500

    Update Model.py

--- Model.py
+++ Model.py
@@ -9,7 +9,7 @@ import pickle
 
 
 def load_data(label):
-    df = pd.read_pickle(r'static\Data\dataframe.pickle')
+    df = pd.read_pickle(r'./static/Data/dataframe.pickle')
     print(df.columns.get_loc('new_cases'))
     y = df['new_cases']
     X = df.drop(columns=['new_cases'])
@@ -25,18 +25,18 @@ def train_xgb():
     d_mat_train = xgb.DMatrix(X_train, y_train)
     params = {'eta': 0.1, 'seed': 0, 'subsample': 0.8, 'colsample_bytree': 0.8, 'objective': 'reg:squarederror',
               'max_depth': 5, 'alpha': 10, 'min_child_weight': 1}
-    with open(r'.\static\Models\model_params.pickle', 'wb') as write:
+    with open(r'./static/Models/model_params.pickle', 'wb') as write:
         pickle.dump(params, write, protocol=pickle.HIGHEST_PROTOCOL)
     booster = xgb.train(params, d_mat_train)
     d_mat_test = xgb.DMatrix(X_test)
-    booster.save_model(r'static\Models\xgb_model.json')
+    booster.save_model(r'./static/Models/xgb_model.json')
 
 def train_svm():
     X, y = load_data(label=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=151)
     regressor = SVR()
     regressor.fit(X_train,y_train)
-    with open(r'static\Models\model_svm.pickle', 'wb') as write:
+    with open(r'./static/Models/model_svm.pickle', 'wb') as write:
         pickle.dump(regressor, write, protocol=pickle.HIGHEST_PROTOCOL)
 
 def train_rfr():
@@ -44,7 +44,7 @@ def train_rfr():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=151)
     regressor = RandomForestRegressor()
     regressor.fit(X_train, y_train)
-    with open(r'static\Models\model_rfr.pickle', 'wb') as write:
+    with open(r'./static/Models/model_rfr.pickle', 'wb') as write:
         pickle.dump(regressor, write, protocol=pickle.HIGHEST_PROTOCOL)
 
 
@@ -53,7 +53,7 @@ def train_dTree():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=151)
     regressor = DecisionTreeRegressor()
     regressor.fit(X_train, y_train)
-    with open(r'static\Models\model_dtree.pickle', 'wb') as write:
+    with open(r'./static/Models/model_dtree.pickle', 'wb') as write:
         pickle.dump(regressor, write, protocol=pickle.HIGHEST_PROTOCOL)
 
 def train_knn():
@@ -61,25 +61,25 @@ def train_knn():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=151)
     regressor = KNeighborsRegressor(n_neighbors=2)
     regressor.fit(X_train, y_train)
-    with open(r'static\Models\model_knn.pickle', 'wb') as write:
+    with open(r'./static/Models/model_knn.pickle', 'wb') as write:
         pickle.dump(regressor, write, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def test(Country,Model):
     preds = 0
     if Model.lower() == 'xgb':
-        file = open(r'static\Models\model_params.pickle', 'rb')
+        file = open(r'./static/Models/model_params.pickle', 'rb')
         params = pickle.load(file)
         file.close()
         bst = xgb.Booster(params)
-        bst.load_model(r'static\Models\xgb_model.json')
+        bst.load_model(r'./static/Models/xgb_model.json')
         index = get_index(Country)
         X = load_data(False)
         X_test = X[X.location == index].iloc[-1].values.reshape(1, -1)
         X_test = xgb.DMatrix(X_test)
         preds = bst.predict(X_test)[0]
     elif Model.lower() == 'svm':
-        file = open(r'static\Models\model_svm.pickle', 'rb')
+        file = open(r'./static/Models/model_svm.pickle', 'rb')
         regressor = pickle.load(file)
         file.close()
         index = get_index(Country)
@@ -116,7 +116,7 @@ def test(Country,Model):
 
 def get_index(Country):
     Country = Country.lower()
-    file = open('static\Data\country_mapper.pickle', 'rb')
+    file = open('./static/Data/country_mapper.pickle', 'rb')
     mapper = pickle.load(file)
     file.close()
     return mapper[Country]
